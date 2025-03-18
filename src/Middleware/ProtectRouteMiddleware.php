d8a7c005e8a9668bbb8137bddb782c3d6e4f3e35
TheRealMkadmi <33043525+TheRealMkadmi@users.noreply.github.com>
2025-03-18 14:00:16 +0000

    Fix styling

--- src/Middleware/ProtectRouteMiddleware.php
+++ src/Middleware/ProtectRouteMiddleware.php
@@ -4,15 +4,15 @@ namespace TheRealMkadmi\Citadel\Middleware;
 
 use Illuminate\Support\Facades\Log;
 
-
 class ProtectRouteMiddleware
 {
     public function handle($request, \Closure $next)
     {
         if (app()->hasDebugModeEnabled()) {
-            Log::info("Middleware is running");
+            Log::info('Middleware is running');
             Log::info($request->getFingerprint());
         }
+
         return $next($request);
     }
 }
